0f11094f195013022d6a015634d932fb2de1d1f0
root <franklinchilaka94@gmail.com>
2023-04-03 05:57:06 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+int pop_listint(listint_t **head)
+{
+	listint_t *start;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	start = *head;
+	*head = start->next;
+	n = start->n;
+	free(start);
+	return (n);
+}
+
