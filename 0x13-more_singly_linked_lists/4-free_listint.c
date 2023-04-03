b640a83f0518022d27e163ee9b0a34a8b74daf3c
root <franklinchilaka94@gmail.com>
2023-04-03 05:52:48 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
