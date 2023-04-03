7f5f22bcc0371db24626ba2d87c2f9eab2174d34
root <franklinchilaka94@gmail.com>
2023-04-03 14:42:26 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,1 +1,1 @@
-law
+0x13-more_singly_linked_lists
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,14 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stddef.h>
-
-
+#include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ */
 typedef struct listint_s
 {
 	int n;
@@ -25,6 +30,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-int _putchar(char c);
 
 #endif
